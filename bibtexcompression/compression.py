a805a2d7aa4c681f6837d18f0e71211b630d3bd8
Marcin Copik <mcopik@gmail.com>
2022-09-20 19:40:46 +0200

    Add mapping known conferences

--- bibtexcompression/compression.py
+++ bibtexcompression/compression.py
@@ -1,5 +1,7 @@
 
+import json
 import logging
+import os
 import re
 from dataclasses import dataclass
 from typing import Optional
@@ -15,6 +17,33 @@ class Settings:
     remove_pages: bool
     remove_year: bool
 
+class ConferenceMappings:
+
+    def __init__(self, path: str):
+
+        with open(path, 'r') as conference_data:
+            self._data = json.load(conference_data)
+
+    @property
+    def conferences(self):
+        return self._data 
+
+FILEPATH = os.path.dirname(os.path.realpath(__file__))
+conference_mappings = ConferenceMappings(os.path.join(FILEPATH, 'conferences.json'))
+
+def match_conference(title) -> Optional[str]:
+
+    """
+        Most of the names are in the form 'Proceedings of X', but there are some minor,
+        stylistic differences - thus we can't easily extract the conference name with regex.
+        Thus, it's easier to go over all mappings.
+    """
+    for conference, short_name in conference_mappings.conferences.items():
+        if conference in title:
+            return short_name
+
+    return None
+
 def extract_series(title) -> Optional[str]:
 
     """
@@ -23,12 +52,33 @@ def extract_series(title) -> Optional[str]:
         BlaBla is what we need to extract
     """
 
-    regex = r'[0-9]+ [a-zA-z/ ]+ \((.*)\)'
+    regex = r'[0-9]+ [0-9a-zA-z/, ]+ \((.*)\)'
     match_regex = re.search(regex, title)
     if match_regex:
         return match_regex.group(1)
     else:
         return None
+
+def compress_proceedings_name(entry) -> Optional[str]:
+
+    if 'publisher' in entry and 'USENIX' in entry['publisher']:
+        proceedings_name = usenix.extract_series(entry)
+        return proceedings_name
+
+    if 'series' in entry:
+        return entry['series']
+
+    # Try to extract series name with regex
+    extracted_series = extract_series(entry['booktitle'])
+    if extracted_series:
+        return extracted_series
+
+    # Try to match the booktitle to a known conference name
+    matched_name = match_conference(entry['booktitle'])
+    if matched_name:
+        return matched_name
+
+    return None
 
 def compress_proceedings(entry, settings: Settings):
 
@@ -37,29 +87,20 @@ def compress_proceedings(entry, settings: Settings):
     # remove the proceedings title, leaving only conference name
     if settings.remove_proceedings:
 
-        if 'publisher' in entry and 'USENIX' in entry['publisher']:
-            proceedings_name = usenix.extract_series(entry)
+        extracted_series = compress_proceedings_name(entry)
+        if extracted_series is not None:
+
             proceedings_name_key = "series"
-            compressed[proceedings_name_key] = proceedings_name
+            compressed[proceedings_name_key] = extracted_series
+
         else:
 
-            if 'series' in entry:
-                proceedings_name_key = "series"
-                compressed[proceedings_name_key] = entry[proceedings_name_key]
-            else:
-                proceedings_name_key = ""
-                extracted_series = extract_series(entry['booktitle'])
-
-                if extracted_series:
-                    proceedings_name_key = "series"
-                    compressed[proceedings_name_key] = extracted_series
-                else:
-                    proceedings_name_key = "booktitle"
-                    compressed[proceedings_name_key] = entry[proceedings_name_key]
-                    logging.warning(f"Proceedings, entry {entry['ID']}, 'series' not found, querying for series in 'booktitle' failed, skipping removing proceedings")
-
+            proceedings_name_key = "booktitle"
+            compressed[proceedings_name_key] = entry[proceedings_name_key]
+            logging.warning(f"Proceedings, entry {entry['ID']}, 'series' not found, querying for series in 'booktitle' failed, skipping removing proceedings")
     else:
         proceedings_name_key = "booktitle"
+        compressed[proceedings_name_key] = entry[proceedings_name_key]
 
     # we do not apply customization during parsing because writer later fails
     # e.g. author customization creates a list and writer expects a string only
